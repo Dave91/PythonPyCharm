2f09783a464ad366f97d906f2a6080ebee53a1a8
Dave <thecothineal@gmail.com>
2021-12-14 10:19:52 +0100

    + kivy gui

---
+++ KivyApps/KVTutorials/hello.py
@@ -0,0 +1,58 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+
+class SayHello(App):
+    # jelen példában az init-rész nem feltétlenül szükséges
+    def __init__(self):
+        super().__init__()
+        self.window = None
+        self.greeting = None
+        self.user = None
+        self.button = None
+
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+
+        # img
+        self.window.add_widget(Image(source="hello2.png"))
+
+        # label
+        self.greeting = Label(
+                        text="What's your name?",
+                        font_size=18,
+                        color="#00FFCE")
+        self.window.add_widget(self.greeting)
+
+        # txt input
+        self.user = TextInput(
+                    multiline=False,
+                    padding_y=(20, 20),
+                    size_hint=(1, 0.5))
+        self.window.add_widget(self.user)
+
+        # btn
+        self.button = Button(
+                    text="Greet!",
+                    size_hint=(1, 0.5),
+                    bold=True,
+                    background_color="#00FFCE",
+                    background_normal="")
+        self.button.bind(on_press=self.callback)
+        self.window.add_widget(self.button)
+
+        return self.window
+
+    def callback(self, inst):
+        self.greeting.text = f"Hello {self.user.text} !"
+
+
+if __name__ == "__main__":
+    SayHello().run()
--- TkApps/Calendar/calendapp.py
+++ TkApps/Calendar/calendapp.py
@@ -1,29 +1,62 @@
 import tkinter as tk
 from tkcalendar import Calendar
+from datetime import date, datetime
 
 
 class App(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+        self.configure(width=400, height=600, bg="beige")
         self.pack()
+
         self.calbg = Calendar(self, selectmode="day", year=2021, month=12, day=10)
-        self.calbg.pack(pady=20)
+        self.calbg.pack(pady=10)
 
         # btn, lab, etc
-        tk.Button(self, text="Add Event")
-        tk.Button(self, text="Get Date", command=self.grad_date).pack(pady=20)
-        self.datesel = tk.Label(self, text="")
-        self.datesel.pack(pady=20)
+        self.showupc_btn = tk.Button(self, text="Show Upcoming Events")
+        self.showupc_btn.pack(pady=10)
+        self.addev_btn = tk.Button(self, text="Add Event")
+        self.addev_btn.pack(pady=10)
+        self.disptext = tk.Text(self)
+        self.disptext.pack(pady=10)
+
+        self.events = self.get_events()
+        self.today = date.today()
+        self.disp_notif()
+
+    @staticmethod
+    def get_events():
+        events_list = []
+        with open("data/events.txt") as file:
+            for line in file:
+                line = line.rstrip("\n")
+                ce = line.split(";")
+                ed = datetime.strptime(ce, "%y/%m/%d").date()
+                ce[1] = ed
+                events_list.append(ce)
+            return events_list
+
+    def disp_notif(self):
+        for event in self.events:
+            en = event[0]
+            du = self.diff(event[1], self.today)
+            display = f"It is {du} days until {en} !\n"
+            self.disptext.insert(display, tk.END)
+
+    @staticmethod
+    def diff(d1, d2):
+        tb = str(d1, d2)
+        nod = tb.split(" ")
+        return nod[0]
 
     def grad_date(self):
         dateselnew = self.calbg.get_date()
-        self.datesel.config(text="Selected Date is: " + dateselnew)
 
 
 if __name__ == "__main__":
     gui = tk.Tk()
     gui.config(background="beige")
-    gui.title("CALENDAR")
-    gui.geometry("400x400")
+    gui.title("My Event Calendar")
+    gui.geometry("400x600")
     App(gui)
     gui.mainloop()
