0bc587580f01ecaadacb276cae6abd9baa328732
Dave <thecothineal@gmail.com>
2022-02-08 18:15:55 +0100

    Ant game: can be improved, rapid clicks cause bug

--- PyGames/Ants Game/antpicnic.py
+++ PyGames/Ants Game/antpicnic.py
@@ -14,7 +14,7 @@ class Pathfinder:
         self.path = []
 
     def create_path(self, dx, dy):
-        if has_end and dx is not None:
+        if dx is not None:
             start = self.grid.node(400 // 50, 350 // 50)
             end = self.grid.node(dx // 50, dy // 50)
 
@@ -27,8 +27,8 @@ class Pathfinder:
             steps = self.path[0]
             nodes = []
             for step in range(len(steps)):
-                nx = (steps[step][0] * 50) + 25
-                ny = (steps[step][1] * 50) + 25
+                nx = steps[step][0] * 50
+                ny = steps[step][1] * 50
                 nodes.append((nx, ny))
             return nodes
 
@@ -56,9 +56,7 @@ class Ants(pygame.sprite.Sprite):
 
         self.node_index = 0
         self.nodes_to_go = nodes
-        self.can_go = False
-        if self.nodes_to_go:
-            self.can_go = True
+        self.has_end = False
 
     def animation_state(self):
         self.frame_index += 0.5
@@ -67,34 +65,55 @@ class Ants(pygame.sprite.Sprite):
         self.image = self.ant_walk_anim[int(self.frame_index)]
 
         if self.nodes_to_go:
-            try:
-                self.node_index += 2
-                if (self.node_index // 10) > len(self.nodes_to_go):
+            if not self.has_end:
+                try:
+                    self.node_index += 2
+                    if (self.node_index // 10) == len(self.nodes_to_go) + 1:
+                        self.has_end = True
+                        self.node_index = 0
+                        self.nodes_to_go.reverse()
+                    else:
+                        nind = self.node_index // 10
+                        node = self.nodes_to_go[nind]
+                        self.rotate_ant(nind + 1)
+                        self.rect.x = node[0]
+                        self.rect.y = node[1]
+                except IndexError:
                     return
-                else:
-                    node = self.nodes_to_go[self.node_index // 10]
-                    self.rect.x = node[0]
-                    self.rect.y = node[1]
-            except IndexError:
-                return
+            else:
+                try:
+                    self.node_index += 2
+                    if (self.node_index // 10) == len(self.nodes_to_go) + 1:
+                        self.has_end = False
+                        self.node_index = 0
+                        self.kill()
+                    else:
+                        nind = self.node_index // 10
+                        node = self.nodes_to_go[nind]
+                        self.rotate_ant(nind + 1)
+                        self.rect.x = node[0]
+                        self.rect.y = node[1]
+                except IndexError:
+                    return
         else:
             return
-            mdir = random.choice(["L", "R", "U", "D"])
-            if mdir == "L":
-                for m in range(4):
-                    self.rect.x -= 2
-            elif mdir == "R":
-                self.image = pygame.transform.rotate(self.image, 180)
-                for m in range(4):
-                    self.rect.x += 2
-            elif mdir == "U":
-                self.image = pygame.transform.rotate(self.image, 90)
-                for m in range(4):
-                    self.rect.y -= 2
-            else:
-                self.image = pygame.transform.rotate(self.image, -90)
-                for m in range(4):
-                    self.rect.y += 2
+
+    def rotate_ant(self, ni):
+        mdirs = {(0, 0): "N", (-1, 0): "L", (1, 0): "R", (0, -1): "U", (0, 1): "D"}
+        try:
+            node = self.nodes_to_go[ni]
+            mdir = mdirs[((node[0] - self.rect.x) // 50, (node[1] - self.rect.y) // 50)]
+            if mdir != "N":
+                if mdir == "L":
+                    pass
+                elif mdir == "R":
+                    self.image = pygame.transform.rotate(self.image, 180)
+                elif mdir == "U":
+                    self.image = pygame.transform.rotate(self.image, 90)
+                else:
+                    self.image = pygame.transform.rotate(self.image, -90)
+        except KeyError:
+            return
 
     def update(self):
         self.animation_state()
@@ -126,16 +145,19 @@ class Food(pygame.sprite.Sprite):
             pass
 
 
-def display_score():
-
-    '''score_surf = game_font.render(
-        f'Time: {current_time} secs, stars: {stars}', False, (64, 64, 64))
-    score_rect = score_surf.get_rect(center=(400, 50))
-    screen.blit(score_surf, score_rect)'''
+def display_intro(surface):
+    surface.fill((255, 255, 255))
+    logo = pygame.image.load("assets/logo.png").convert_alpha()
+    logo_rect = logo.get_rect(center=(400, 250))
+    surface.blit(logo, logo_rect)
+    intro_font = pygame.font.SysFont('Calibri', 32)
+    intro_surf = intro_font.render("Press Space to begin...", False, (64, 64, 64))
+    intro_rect = intro_surf.get_rect(center=(400, 350))
+    surface.blit(intro_surf, intro_rect)
+    pygame.display.update()
 
 
 def collision_sprite():
-
     if pygame.sprite.groupcollide(ants_group, food_group, False, True):
         return True
     else:
@@ -153,20 +175,21 @@ def init_dots():
 
 
 def get_input(pathfinder):
-    global has_end
+    global intro
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
-            print(event)
+            if event.key == pygame.K_SPACE:
+                if intro:
+                    intro = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 m_pos = pygame.mouse.get_pos()
                 dx = m_pos[0]
                 dy = m_pos[1]
                 food_group.add(Food(dx, dy))
-                has_end = True
                 pathfinder.create_path(dx, dy)
                 nodes = pathfinder.draw_path()
                 ants_group.add(Ants(400, 350, nodes))
@@ -176,7 +199,7 @@ ants_group = pygame.sprite.Group()
 food_group = pygame.sprite.Group()
 
 running = True
-has_end = False
+intro = True
 
 
 def main():
@@ -200,8 +223,11 @@ def main():
 
     pathfinder = Pathfinder(matrix, surface)
 
+    while intro:
+        get_input(pathfinder)
+        display_intro(surface)
+
     while running:
-        # surface.fill((255, 255, 255))
         surface.blit(bg, (0, 0))
         pygame.draw.circle(surface, (0, 0, 0), (425, 375), 25)
         get_input(pathfinder)
@@ -210,11 +236,10 @@ def main():
         food_group.draw(surface)
         food_group.update()
 
-        game_active = collision_sprite()
+        collision_sprite()
         pathfinder.update()
         pygame.display.update()
-        clock.tick(30)
-        # count_pops(total_pop, d1_pop, d2_pop, d3_pop, d4_pop)
+        clock.tick(60)
 
     pygame.quit()
     exit()
